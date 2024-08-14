96539dae1fcb0e01a86a9c356261f8b31fc5c73c
Arthur Correia <arthurc.estudos3@gmail.com>
2024-08-13 21:44:58 -0300

    projeto incial

---
+++ app.js
@@ -0,0 +1,22 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio!';
+
+function click01(){
+    console.log('o botão foi clicado');
+}
+
+function click02(){
+    console.log('eu amo JS');
+}
+
+function click03(){
+    cidade = prompt('Nome de uma cidade do Brasil: ');
+    alert(`Eu já estive em ${cidade} e lembrei de você!`);
+}
+
+function click04() {
+    valor1 = parseInt(prompt('número um: '));
+    valor2 = parseInt(prompt('número dois'));
+    soma = valor1 + valor2;
+    alert(soma);
+}
